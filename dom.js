6f86060cb738d626c645af7e861007989a44314d
Brian Santos <brianmsantos81@gmail.com>
2019-07-27 20:43:28 -0700

    need to work on dom form

--- dom.js
+++ dom.js
@@ -14,4 +14,20 @@ items.forEach((item) => console.log(item));
 // Manipulating the DOM
 const ul = document.querySelector('.items');
 // ul.remove();
-ul.firstElementChild.textContent = 'Hello!!'
+ul.firstElementChild.textContent = 'Hello!!';
+ul.children[1].innerText = 'Brian';
+ul.lastElementChild.innerHTML = '<h1>Hello again!!</h1>';
+
+const btn = document.querySelector('.btn');
+// btn.style.background = 'red';
+
+// Events
+btn.addEventListener('click', (e)=> {
+    e.preventDefault();
+    document.querySelector('#my-form').style.background = '#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>ß'
+})
+
+const myForm = document.querySelector('')
+// Start at 1:31:57
