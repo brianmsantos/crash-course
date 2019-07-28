860d7eaf922ddf0125649ac9dc4de5395b80d1e9
Brian Santos <brianmsantos81@gmail.com>
2019-07-27 19:15:01 -0700

    prototype

--- main.js
+++ main.js
@@ -127,12 +127,14 @@ function Person (firstName, lastName, dob){
     this.firstName = firstName;
     this.lastName = lastName;
     this.dob = new Date(dob);
-    this.getBirthYear = function (){
-      return this.dob.getFullYear();
-    }
-    this.getFullName =function(){
-      return `${this.firstName} ${this.lastName}`
-    }
+}
+
+Person.prototype.getBirthYear = function() {
+  return this.dob.getFullYear();
+}
+
+Person.prototype.getFullName = function() {
+  return `${this.firstName} ${this.lastName}`;
 }
 
 //Instantiate object
@@ -147,4 +149,4 @@ console.log(person2.dob.getFullYear());
 console.log(person1.getBirthYear());
 console.log(person1.getFullName());
 
-//Start at 1:04:51 when returning 
+console.log(person1.getFullName());
