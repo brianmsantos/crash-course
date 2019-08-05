012a9f6d4a8980680b8f0746b25459b113c5c4ee
Brian Santos <brianmsantos81@gmail.com>
2019-08-05 10:29:33 -0700

    validation added

--- dom.js
+++ dom.js
@@ -7,27 +7,44 @@
 // // Multiple Element Selectors
 // console.log(document.querySelectorAll('.item'));
 
-// Loop through
-const items = document.querySelectorAll('.item');
-items.forEach((item) => console.log(item));
+// // Loop through
+// const items = document.querySelectorAll('.item');
+// items.forEach((item) => console.log(item));
 
-// Manipulating the DOM
-const ul = document.querySelector('.items');
-// ul.remove();
-ul.firstElementChild.textContent = 'Hello!!';
-ul.children[1].innerText = 'Brian';
-ul.lastElementChild.innerHTML = '<h1>Hello again!!</h1>';
+// // Manipulating the DOM
+// const ul = document.querySelector('.items');
+// // ul.remove();
+// ul.firstElementChild.textContent = 'Hello!!';
+// ul.children[1].innerText = 'Brian';
+// ul.lastElementChild.innerHTML = '<h1>Hello again!!</h1>';
 
-const btn = document.querySelector('.btn');
-// btn.style.background = 'red';
+// const btn = document.querySelector('.btn');
+// // btn.style.background = 'red';
 
-// Events
-btn.addEventListener('click', (e)=> {
+// // Events
+// btn.addEventListener('click', (e)=> {
+//     e.preventDefault();
+//     document.querySelector('#my-form').style.background = '#ccc';
+//     document.querySelector('body').classList.add('bg-dark');
+//     document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>ß'
+// })
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
     e.preventDefault();
-    document.querySelector('#my-form').style.background = '#ccc';
-    document.querySelector('body').classList.add('bg-dark');
-    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>ß'
-})
 
-const myForm = document.querySelector('')
-// Start at 1:31:57
+    if(nameInput.value === '' || emailInput.value === ''){
+            alert ('Please enter fields');
+        } else {
+            console.log('success');
+        }
+    
+}
+
